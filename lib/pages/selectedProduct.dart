aea64b1aff3e4ba2c0c0f4f8e6ea6f91c6f43589
shanelmanimtim <21-12973@g.batstate-u.edu.ph>
2024-07-17 11:34:40 +0800

    Nakakapagadd na

---
+++ lib/pages/selectedProduct.dart
@@ -0,0 +1,111 @@
+import'package:flutter/material.dart';
+
+import '../services/product.dart';
+
+
+class selectedProduct extends StatefulWidget {
+  final Product product;
+  const selectedProduct({super.key,required this.product});
+
+  @override
+
+  State<selectedProduct> createState() => _selectedProductState(product: product);
+}
+
+class _selectedProductState extends State<selectedProduct> {
+  final Product product;
+  late double totalAmount = product.price;
+  int numbersOfOrders =1;
+  _selectedProductState({required this.product});
+
+  @override
+  void initState(){
+    super.initState();
+    totalAmount = product.price;
+  }
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text(
+          'Order',
+          style: TextStyle(
+            color: Colors.white,
+            fontWeight: FontWeight.bold,
+          ),
+        ),
+      ),
+      body: Column(
+
+        children: [
+          SizedBox(height: 20.0,),
+          Text(widget.product.productName,
+            style: TextStyle(
+              fontSize: 40.0,
+            ),
+          ),
+          SizedBox(height: 20.0,),
+          Text(widget.product.description,
+            style: TextStyle(
+              fontSize: 20.0,
+            ),
+          ),
+          SizedBox(height: 20.0,),
+          Row(
+            mainAxisAlignment: MainAxisAlignment.spaceBetween,
+            children: [
+              Text(
+                '₱ ${totalAmount.toString()}',
+                style: TextStyle(
+                  fontSize: 20.0,
+                ),
+              ),
+              Row(
+                children: [
+                  IconButton(
+                    onPressed: (){
+                      setState(() {
+                        if(numbersOfOrders > 1){
+                          numbersOfOrders -= 1;
+                          totalAmount = product.price * numbersOfOrders;
+                        }
+                      });
+                    },
+                    icon: Icon(Icons.remove),
+                  ),
+                  Text(
+                    numbersOfOrders.toString(),
+                    style: TextStyle(
+                      fontSize: 20.0,
+                    ),
+                  ),
+                  IconButton(
+                    onPressed:(){
+                      setState(() {
+                        numbersOfOrders += 1;
+                        totalAmount = product.price * numbersOfOrders;
+                      });
+                    },
+                    icon: Icon(Icons.add),
+                  ),
+                ],
+              ),
+            ],
+          ),
+          SizedBox(height: 50,),
+          ElevatedButton(
+            onPressed: (){
+              Navigator.pushNamed(context, '/menu');
+            }
+            , child: Text(
+            '         Add to cart      ',
+            style: TextStyle(
+              fontSize: 15.0,
+              fontWeight: FontWeight.bold,
+            ),
+          ),
+          ),
+        ],
+      ),
+    );
+  }
+}
