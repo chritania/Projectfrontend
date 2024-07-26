1119a356bdffaec8639002c018c341ef493d9cf2
chritania <chritaniap@gmail.com>
2024-07-26 15:26:20 +0800

    cart

--- lib/pages/Cart.dart
+++ lib/pages/Cart.dart
@@ -1,106 +1,81 @@
+import 'dart:convert';
 import 'package:flutter/material.dart';
+import 'package:shared_preferences/shared_preferences.dart';
+import 'package:http/http.dart' as http;
+import '../services/product.dart';
 
-class Cart extends StatelessWidget {
+class Cart extends StatefulWidget {
   const Cart({Key? key}) : super(key: key);
 
-  Future<void> _showOrderFormDialog(BuildContext context) async {
-    final _formKey = GlobalKey<FormState>();
-    final _nameController = TextEditingController();
-    final _addressController = TextEditingController();
-    final _landmarkController = TextEditingController();
-    final _phoneController = TextEditingController();
+  @override
+  State<Cart> createState() => _CartState();
+}
 
-    return showDialog<void>(
-      context: context,
-      barrierDismissible: false, // Prevents dismissing the dialog by tapping outside
-      builder: (BuildContext context) {
-        return AlertDialog(
-          backgroundColor: Colors.white70,
-          title: const Text('Order Form'),
-          content: SingleChildScrollView(
-            child: Form(
-              key: _formKey,
-              child: Column(
-                crossAxisAlignment: CrossAxisAlignment.start,
-                children: <Widget>[
-                  TextFormField(
-                    controller: _nameController,
-                    decoration: const InputDecoration(
-                      labelText: 'Name',
-                    ),
-                    validator: (value) {
-                      if (value == null || value.isEmpty) {
-                        return 'Please enter your name';
-                      }
-                      return null;
-                    },
-                  ),
-                  TextFormField(
-                    controller: _addressController,
-                    decoration: const InputDecoration(
-                      labelText: 'Address',
-                    ),
-                    validator: (value) {
-                      if (value == null || value.isEmpty) {
-                        return 'Please enter your address';
-                      }
-                      return null;
-                    },
-                  ),
-                  TextFormField(
-                    controller: _landmarkController,
-                    decoration: const InputDecoration(
-                      labelText: 'Landmark (Optional)',
-                    ),
-                    validator: (value) {
-                      return null; // Always return null to make the field optional
-                    },
-                  ),
-                  TextFormField(
-                    controller: _phoneController,
-                    decoration: const InputDecoration(
-                      labelText: 'Phone Number',
-                    ),
-                    keyboardType: TextInputType.phone,
-                    validator: (value) {
-                      if (value == null || value.isEmpty) {
-                        return 'Please enter your phone number';
-                      }
-                      return null;
-                    },
-                  ),
-                ],
-              ),
-            ),
-          ),
-          actions: <Widget>[
-            TextButton(
-              child: const Text('Cancel'),
-              onPressed: () {
-                Navigator.of(context).pop();
-              },
-            ),
-            ElevatedButton(
-              child: const Text('Submit'),
-              onPressed: () {
-                if (_formKey.currentState!.validate()) {
-                  // Process the order
-                  Navigator.of(context).pop(); // Close the dialog
+class _CartState extends State<Cart> {
 
-                  // Show the SnackBar with the result
-                  ScaffoldMessenger.of(context).showSnackBar(
-                    const SnackBar(
-                      content: Text('Order submitted'),
-                      duration: Duration(seconds: 2),
-                    ),
-                  );
-                }
-              },
-            ),
-          ],
-        );
-      },
-    );
+  Future<Map<String, dynamic>> _loadCredential() async {
+    final prefs = await SharedPreferences.getInstance();
+    String email = prefs.get('email').toString();
+    String password = prefs.get('password').toString();
+    int userId = int.parse(prefs.get('userId').toString());
+
+    return <String, dynamic>{
+      'email': email,
+      'password': password,
+      'userId' : userId
+    };
+  }
+
+  Future<List<dynamic>> fetchBag() async {
+    List<Map<String, dynamic>> orders = [];
+    try {
+      final userCredentials = await _loadCredential();
+      final basicAuth = base64Encode(utf8.encode('${userCredentials['email']}:${userCredentials['password']}'));
+      final response = await http.get(
+          Uri.parse('http://10.0.2.2:8080/api/v1/bag/${userCredentials['userId']}'),
+          headers: <String, String>{
+            'Authorization' : 'Basic $basicAuth',
+            'Content-Type' : 'application/json'
+          }
+      );
+      print(jsonDecode(response.body));  // Print JSON response for debugging
+      final data = jsonDecode(response.body);
+      for(var item in data){
+        orders.add(<String, dynamic>{
+          'bagId' : item['bagId'],
+          'product' : Product.fromJson(item['product'][0]),
+          'userId' : item['userId'],
+          'numberOfOrder' : item['numberOfOrder']
+        });
+      }
+      return orders;
+    } catch (err) {
+      print(err);
+      return orders;
+    }
+  }
+
+  Future<void> removeItem(int bagId) async {
+    try {
+      final userCredentials = await _loadCredential();
+      final basicAuth = base64Encode(utf8.encode('${userCredentials['email']}:${userCredentials['password']}'));
+      final response = await http.delete(
+          Uri.parse('http://10.0.2.2:8080/api/v1/bag/$bagId'),
+          headers: <String, String>{
+            'Authorization' : 'Basic $basicAuth',
+            'Content-Type' : 'application/json'
+          }
+      );
+      if (response.statusCode == 200) {
+        setState(() {
+          fetchBag();  // Refresh the cart after deleting an item
+        });
+      } else {
+        print('Failed to delete the item');
+      }
+    } catch (err) {
+      print(err);
+    }
   }
 
   @override
@@ -132,27 +107,73 @@ class Cart extends StatelessWidget {
             stops: [0.0, 0.5, 1.0], // Gradient stops for each color
           ),
         ),
-        child: Column(
-          children: [
-            Expanded(
-              child: Center(
-                child: Text(''),
-              ),
-            ),
-            Padding(
-              padding: const EdgeInsets.all(16.0),
-              child: ElevatedButton(
-                onPressed: () {
-                  _showOrderFormDialog(context);
-                },
-                child: const Text('Place Order Now'),
-                style: ElevatedButton.styleFrom(
-                  backgroundColor: Colors.white70, // Background color
-                  foregroundColor: Colors.black, // Text color
-                ),
-              ),
-            ),
-          ],
+        child: FutureBuilder(
+          future: fetchBag(),
+          builder: (context, snapshots) {
+            if (snapshots.connectionState == ConnectionState.waiting) {
+              return Center(
+                child: CircularProgressIndicator(), // Or any other loading indicator
+              );
+            }
+            if (snapshots.hasData) {
+              List bagItems = snapshots.data!;
+              return Column(
+                children: [
+                  Expanded(
+                    child: ListView.builder(
+                      itemCount: bagItems.length,
+                      itemBuilder: (context, index) {
+                        final item = bagItems[index];
+                        return Container(
+                          margin: EdgeInsets.all(10.0),
+                          padding: EdgeInsets.all(10.0),
+                          decoration: BoxDecoration(
+                            color: Colors.white70,
+                            borderRadius: BorderRadius.circular(10.0),
+                            boxShadow: [
+                              BoxShadow(
+                                color: Colors.black26,
+                                blurRadius: 5.0,
+                                offset: Offset(0, 2),
+                              ),
+                            ],
+                          ),
+                          child: ListTile(
+                            title: Text(item['product'].name),
+                            subtitle: Text('Quantity: ${item['numberOfOrder']}'),
+                            trailing: IconButton(
+                              icon: Icon(Icons.remove_circle_outline),
+                              onPressed: () {
+                                removeItem(item['bagId']);
+                              },
+                            ),
+                          ),
+                        );
+                      },
+                    ),
+                  ),
+                  ElevatedButton(
+                    onPressed: () {
+                      print('Button Pressed!');
+                    },
+                    child: Text(
+                      'Place your order',
+                      style: TextStyle(
+                        color: Colors.black,
+                      ),),
+                  ),
+                ],
+              );
+            } else if (snapshots.hasError) {
+              return Center(
+                child: Text('Error: ${snapshots.error}'),
+              );
+            } else {
+              return Center(
+                child: Text('No data available'),
+              );
+            }
+          },
         ),
       ),
     );
